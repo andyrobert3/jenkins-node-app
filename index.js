2b70202415759e7451991b187db3d6f7d2c0c4a0
Matthew Alexander <alexandermatthew12@gmail.com>
2019-05-15 15:09:57 +0800

    logging introduced

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
+	console.log("Hello World");
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -4,5 +4,6 @@ var app = require('../index.js');
 describe('GET /', () => {
     it('respond with hello world', done => {
         request(app).get('/').expect('Hello World', done);
+        console.log("test passed?");
     })
 });
